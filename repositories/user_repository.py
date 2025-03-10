1f1d88dfe10e2098825b89a20aaaf3ef5b165847
Camilo Valencia <camilovalencia.dev@gmail.com>
2025-03-10 16:54:13 -0500

    Corrected repository structure and ORM

--- repositories/user_repository.py
+++ repositories/user_repository.py
@@ -3,37 +3,32 @@ from typing import List, Optional
 from sqlmodel import SQLModel
 from models.user import User
 from repositories.BaseRepository import BaseRepository
-from schemas.user import UserCreate, UserResponse
 
 class UserRepository(BaseRepository):
-    def __init__(self, db:Session):
+    def __init__(self, db: Session):
         self.db = db
 
-    
-    def create(self, obj_data: UserCreate) -> UserResponse:
-        user = User(**obj_data.model_dump())
-        self.db.add(user)
+    def create(self, obj_data: User) -> User:
+        self.db.add(obj_data)
         self.db.commit()
-        self.db.refresh(user)
-        return UserResponse.model_validate(user)
-    
-    def get_by_id(self, obj_id: int) -> Optional[UserResponse]:
-        return self.db.query(User).filter(User.id == obj_id).first()
-    
+        self.db.refresh(obj_data)
+        return obj_data
 
-    def get_all(self) -> List[UserResponse]:
-        users = self.db.query(User.id, User.name, User.email).all()  
-        return [UserResponse(id=u.id, name=u.name, email=u.email) for u in users]
-    
+    def get_by_id(self, obj_id: int) -> Optional[User]:
+        return self.db.query(User).filter(User.id == obj_id).first()
+
+    def get_all(self) -> List[User]:
+        return self.db.query(User).all()
+
     def update(self, obj_id: int, obj_data: dict) -> Optional[User]:
         user = self.get_by_id(obj_id)
         if user:
             for key, value in obj_data.items():
-                setattr(user,key,value)
+                setattr(user, key, value)
             self.db.commit()
             self.db.refresh(user)
         return user
-    
+
     def delete(self, obj_id: int) -> bool:
         user = self.get_by_id(obj_id)
         if user:
---
+++ services/user_service.py
@@ -0,0 +1,23 @@
+from repositories.user_repository import UserRespository
+from schemas.user import UserCreate, UserResponse
+from models.user import User
+from fastapi import HTTPException
+
+
+class UserService:
+    def __init__(self, user_repository: UserRespository):
+        self.user_repository = user_repository
+    
+    def create_user(self, user_data: UserCreate) -> UserResponse:
+        user = User(**user_data.model_dump()) 
+        created_user = self.user_repo.create(user)
+        return UserResponse.model_validate(created_user) 
+    
+    def get_user(self, user_id:int) -> UserResponse | None:
+        user = self.user_repository.get_by_id(user_id)
+        return UserResponse.model_validate(user) if user else None
+
+    def delete_user(self, user_id:int) -> bool:
+        if not self.user_repository.delete(user_id):
+            raise HTTPException(status_code=404, detail="User not found")
+        return True
